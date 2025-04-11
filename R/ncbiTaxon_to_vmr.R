e3067f9a330b523b041436349d9a7eab12e4466b
ejurga <ejacc@protonmail.com>
2025-04-11 13:50:33 -0400

    adds seperate function for downloading and loading ncbi ont

--- R/ncbiTaxon_to_vmr.R
+++ R/ncbiTaxon_to_vmr.R
@@ -1,21 +1,32 @@
+#' Download and load the NCBITaxon ontology
+#'
+download_and_load_ncbiTaxon <- function(){
+  message("Downloading")   
+  file <- tempfile() 
+  curl::curl_download(url = "https://purl.obolibrary.org/obo/ncbitaxon.obo", destfile = file, quiet = FALSE)
+  message("Loading ontology into R (takes some time), and requires > 15Gb mem")
+  ncbi <- ontologyIndex::get_OBO(file, extract_tags = "everything")
+  return(ncbi)
+}
+
+
+
 #' Get full NCBITaxon ontology and add bacterial genera and species to microbes table
 #' 
 #' @param db DBI connection to VMR
-#' @param file File to download the obo file to 
+#' @param ncbi ontology loaded into R
 #' @param commit Commit to the VMR?
 #' 
-#' @export
-append_ncbi_taxon_ontology_to_microbes <- function(db, file, insert = FALSE){
-
-  if (!file.exists(file)){
-    curl::curl_download(url = "https://purl.obolibrary.org/obo/ncbitaxon.obo", destfile = file, quiet = FALSE)
-  }
-  
-  message("Loading ontology into R (takes some time)")
-  ncbi <- ontologyIndex::get_OBO(file, extract_tags = "everything")
+append_ncbi_taxon_ontology_to_microbes <- function(db, ncbi, insert = FALSE){
   
   # Get Bacillati, Fusobacteriati, Pseudomonadati 
   bacteria <- ontologyIndex::get_descendants(ncbi, roots = c("NCBITaxon:1783272","NCBITaxon:3384189","NCBITaxon:3379134"), exclude_roots = TRUE)
+ 
+  # Remove uncertain taxa
+  unc <- grep(x=ncbi$name[bacteria], "unclassified")
+  env <- grep(x=ncbi$name[bacteria], "environmental")
+  inc_sed <- grep(x=ncbi$name[bacteria], "incertae sedis")
+  uncBac <- ontologyIndex::get_descendants(ncbi, roots = ncbi$id[bacteria][c(unc, env, inc_sed)], exclude_roots = FALSE)
   
   df <- tidyr::tibble(ontology_id = bacteria, scientific_name = ncbi$name[bacteria], property = ncbi$property_value[bacteria])
   x <- ncbi$property_value[bacteria]
@@ -25,10 +36,9 @@ append_ncbi_taxon_ontology_to_microbes <- function(db, file, insert = FALSE){
   
   df_gs <-
     df |> 
-    dplyr::filter(has_rank %in% c("NCBITaxon:genus", "NCBITaxon:species", "NCBITaxon:subgenus", "NCBITaxon:subspecies",  
-                           "NCBITaxon:strain", "NCBITaxon:serotype", "NCBITaxon:biotype", "NCBITaxon:varietas",   
-                           "NCBITaxon:serogroup")) |>
-    dplyr::select(ontology_id, scientific_name) 
+    dplyr::filter(has_rank %in% c("NCBITaxon:genus", "NCBITaxon:species", "NCBITaxon:subgenus", "NCBITaxon:subspecies")) |>
+    dplyr::select(ontology_id, scientific_name) |>
+    dplyr::filter(!ontology_id %in% uncBac)
   
   dbOrgs <- DBI::dbReadTable(db, "microbes")
   
