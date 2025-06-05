9e26a626b21ca5c1d2d4aa62b2ef46e96543fe6c
ejurga <ejacc@protonmail.com>
2025-06-05 15:36:24 -0400

    removes func that enabled mass working on arguments from a function

--- R/insert_helpers.R
+++ R/insert_helpers.R
@@ -71,22 +71,6 @@ insert_into_multi_choice_table <- function(db, ids, vals, table, is_ontology = F
   }
 }
 
-#' Convert a function's arguments into a list of vectors of same length
-#' 
-#' Input the `environment()` of a function to return the parameters as a 
-#' list of equal length vectors, without the db argument that is common to
-#' all the insert functions.
-#' 
-#' @param env the [environment] call of a function
-#' @return a list of the functions arguments, minus the DBI connection argument
-#' 
-sql_args_to_uniform_list <- function(env){
-  argg <- c(as.list(env))
-  argg <- argg[!names(argg)=="db"]
-  argg <- if_len_one_rep(argg)
-  return(argg)
-}
-
 #' Convert the vectors of ontology terms into their corresponding VMR ids
 #' 
 #' Given a list of vectors that are to be input into the VMR, convert the 
