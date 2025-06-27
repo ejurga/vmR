0e821f6f016127e0e1ab82efc26fa42fbb4367f6
ejurga <ejacc@protonmail.com>
2025-06-27 17:30:21 -0400

    brings back critical func that was deleted'

--- R/helpers.R
+++ R/helpers.R
@@ -1,3 +1,40 @@
+#' Insert values into one of the common-format multi-choice metadata tables 
+#' 
+#' Currently, the GRDI schema allows some fields to be populated with multiple
+#' values. These fields are implemented in the database as their own tables.
+#' These tables are all similar in their structure, so this convenience
+#' function quickly populates these tables given the values in the GRDI fields.
+#' The function assumes that the values are separated wither with
+#' "|", or a ";"
+#'
+#' @param db [DBI] connection to VMR
+#' @param ids a vector of the foreign-key ids of the table that the multi-choice 
+#'            table is related by.
+#' @param vals a vector of values, of same length as ids, that are to be inserted 
+#'             as values into the multi-choice table
+#' @param table The name of the destination multi-choice table
+#' @param is_ontology Are the values GRDI ontology terms?
+#' @export
+insert_into_multi_choice_table <- function(db, ids, vals, table, is_ontology = FALSE){
+
+  df_long <- 
+    tibble(id = ids, terms = vals) %>%
+    separate_longer_delim(cols = terms, delim = stringr::regex("\\s{0,1}[|;]\\s{0,1}")) %>%
+    filter(!is.na(terms))
+  
+  if (nrow(df_long)==0){ 
+    message("No values for table ", table)
+  } else { 
+    if (is_ontology==TRUE){
+      message("Inserting into ", table)
+      df_long$ont_ids <- convert_GRDI_ont_to_vmr_ids(db, df_long$terms)
+    }
+    insert_sql <- glue::glue_sql("INSERT INTO", table, "VALUES ($1, $2)", .sep = " ")
+    res <- dbExecute(db, insert_sql, list(df_long$id, df_long$ont_ids))
+    message("Inserted ", res, " record into multi-choice table ", table)
+  }
+}
+
 #' amr_regexes
 #'
 #' Rerturn a vector to filter out AMR columns
