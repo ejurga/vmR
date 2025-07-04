140f59160007e9c88223473b1d2f04e4c208ad7a
ejurga <ejacc@protonmail.com>
2025-07-04 19:09:12 -0400

    Adds sequence blank df

--- R/external_schema.R
+++ R/external_schema.R
@@ -15,16 +15,32 @@ parse_latest_grdi_schema <- function(){
 #' @param nrow number of rows for the dataframe
 #' @return empty dataframe of length nrow
 #' @export
-make_blank_df <- function(nrow = 1, type = c("samples", "isolates")) {
+make_blank_df <- function(nrow = 1, type = c("samples", "isolates", "sequences")) {
   type <- match.arg(type)
   schema <- vmR:::parse_latest_grdi_schema()
   slot_usage <- schema$classes$GRDI$slot_usage
   x <- sapply(FUN = function(x) slot_usage[[x]]$slot_group, X = names(slot_usage))
   
+  ext_fields <- c("experimental_protocol_field",
+                  "experimental_specimen_role_type", 
+                  "nucleic_acid_extraction_method",
+                  "nucleic_acid_extraction_kit",
+                  "sample_volume_measurement_value", 
+                  "sample_volume_measurement_unit",
+                  "residual_sample_status",
+                  "sample_storage_duration_value", 
+                  "sample_storage_duration_unit",
+                  "nucleic_acid_storage_duration_value",
+                  "nucleic_acid_storage_duration_unit")
+  
   if (type=="samples"){
-    cols <- names(x[x %in% c('Sample collection and processing', 'Environmental conditions and measurements', 'Host information', 'Risk assessment information')])
+    sam_cols <- names(x)[x %in% c('Sample collection and processing', 'Environmental conditions and measurements', 'Host information', 'Risk assessment information')]
+    cols <- sam_cols[!sam_cols %in% ext_fields]
   } else if (type=="isolates") {
     cols <- names(x[x=="Strain and isolation information"])
+  } else if (type=="sequences") {
+    seq_cols <- names(x[x=="Sequence information"])
+    cols <- c(ext_fields,seq_cols)
   } else stop("problem!")
   
   df <- as_tibble(matrix(ncol = length(cols), nrow = nrow, dimnames = list(NULL, cols)))
