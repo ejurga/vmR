54220409aad8fcbe38fd19779272091d8bd6304d
ejurga <ejacc@protonmail.com>
2025-07-17 17:31:18 -0400

    removes auto-update function, must be reworked

--- R/irida.R
+++ R/irida.R
@@ -1,50 +1,4 @@
 
-#' Get sequence information from IRIDA for isolates in VMR without this info
-#' 
-#' This function queries the VMR for isolates that have an IRIDA sample ID, and 
-#' with WGS information, but without corresponding IRIDA sequence information 
-#' (that is, irida ids representing the sequence), and attempts to return this 
-#' information from the IRIDA server.
-#' 
-#' @inheritParams get_sample_ids
-#' 
-#' @return A dataframe with VMR sequence records and associated sequence 
-#' information from IRIDA
-#' 
-#' @export
-associate_irida_seqs_with_vmr_isolates <- function(db){
-
-  df <- isolates_with_no_irida_sequencing_ids(db)
-  message("Found ", nrow(df), "records with missing irida sequence info") 
-  
-  irida_seqs <- query_pairs_from_irida(samples = df$irida_sample_id)
-
-  # Initialize columns
-  df$r1_fastq_filename <- NA
-  df$r2_fastq_filename <- NA
-  df$r1_irida_id <- NA
-  df$r2_irida_id <- NA
-  
-  for ( sam in unique(irida_seqs$sample_id) ){
-    in_vmr <- df$irida_sample_id==sam
-    in_irida <- irida_seqs$sample_id==sam
-    if (sum(in_vmr)>1) {
-      message("multiple sequences in database, assign manually for: ", unique(df$irida_sample_id[in_vmr]))
-    } else if ( sum(in_irida)>1 ){
-      message("multiple sequences found in irida, assign manually for: ", unique(irida_seqs$sample_id[in_irida]))
-    } else {
-       x <- irida_seqs[in_irida,]
-       df$r1_fastq_filename[in_vmr] <- x$file_forward
-       df$r2_fastq_filename[in_vmr] <- x$file_reverse
-       df$r1_irida_id[in_vmr] <- x$identifier_forward
-       df$r2_irida_id[in_vmr] <- x$identifier_reverse
-    }
-  } 
-
-  return(df)
-}
-
-
 #' Get isolates that have an IRIDA id but are missing corresponding IRIDA sequence information
 #' 
 #' @inheritParams get_sample_ids
@@ -101,11 +55,9 @@ query_pairs_from_irida <- function(samples){
 #' Update VMR with irida sequence information
 #' 
 #' @inheritParams get_sample_ids
-#' @param df dataframe returned from [associate_irida_seqs_with_vmr_isolates]
 #' 
 #' @export
-update_sequences_with_irida <- function(db, df){
-  upd <- df |> dplyr::filter(!is.na(r1_fastq_filename))
+update_sequences_with_irida <- function(db, seq_id, f_file, r_file, f_id, r_id){
 
   query <- "UPDATE sequencing SET r1_fastq_filename = $1,
                                   r2_fastq_filename = $2,
@@ -113,8 +65,6 @@ update_sequences_with_irida <- function(db, df){
                                         r2_irida_id = $4 
                        WHERE id = $5"
  
-  DBI::dbBegin(db) 
-  n <- dbExecute(db, query, list(upd$r1_fastq_filename, upd$r2_fastq_filename, upd$r1_irida_id, upd$r2_irida_id,  
-                                 upd$sequencing_id))
-  message("updated ", n, " records with irida sequencing info, call DBI::dbCommit when ready")
+  n <- dbExecute(db, query, list(f_file, r_file, f_id, r_id, seq_id))
+  message("updated ", n, " records with irida sequencing info")
 }
